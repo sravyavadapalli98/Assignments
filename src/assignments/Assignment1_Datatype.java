687a382e5390024383ceeec69179e08558b06f82
sravyavadapalli98 <sravyavadapalli98@gmil.com>
2025-05-25 23:37:46 +0530

    Updated Assignment_datatype

--- src/assignments/Assignment1_Datatype.java
+++ src/assignments/Assignment1_Datatype.java
@@ -3,8 +3,39 @@ package assignments;
 public class Assignment1_Datatype {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
-
+	//byte //
+		byte number = 25;
+		System.out.println(number);
+		
+	// short//
+		short value = -31000;
+		System.out.println(value);
+   //Integer//
+		int a =214748;
+		System.out.println(a);
+	//long//
+		long phone=859764552L;
+		System.out.println(phone);
+	// Float//
+		float marks = 78.9F;
+		System.out.println(marks);
+	//Double//
+		double total= 99.555555555555;
+		System.out.println(total);
+	//boolean//
+		boolean statement = true;
+		System.out.println(statement);
+	//char//
+		char name ='S';
+		System.out.println(name);
+	//String//
+		String token ="Sudhclj123";
+		System.out.println(token);
+	
+				
+		
 	}
 
 }
