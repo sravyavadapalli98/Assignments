1f5ea82dba8604f2d82c8bcf25738c6db937005f
sravyavadapalli98 <sravyavadapalli98@gmil.com>
2025-05-27 10:35:34 +0530

    Assignment 3 completed

---
+++ src/assignments/Assignemnet3_Empgroups.java
@@ -0,0 +1,15 @@
+package assignments;
+
+public class Assignemnet3_Empgroups {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Using object//
+		
+		Assignment3_Employee emp = new Assignment3_Employee();
+		System.out.println(emp.name[0]);
+		System.out.println(emp.idNumber[0]);
+
+	}
+
+}
---
+++ src/assignments/Assignment3_Employee.java
@@ -0,0 +1,13 @@
+package assignments;
+
+public class Assignment3_Employee {
+	// using class//
+	String[] name = {"Bharath","ABC","XYZ"};
+	int[] idNumber = {1234, 1235, 1236}; 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   
+	}
+
+}
